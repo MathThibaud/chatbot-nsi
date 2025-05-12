070ea872a38b6d5147055f82682c61a07f1a5a42
Mathieu THIBAUD <94779022+MathThibaud@users.noreply.github.com>
2025-05-12 11:50:49 +0200

    essais

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-import os
-import markdown
-import random
-from openai import OpenAI
+from flask import Flask
 
 app = Flask(__name__)
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-@app.route("/")
-def index():
-    return "✅ Application Flask opérationnelle"
-
-# ajoute ici tes autres routes
+# On importe les routes (dans main.py)
+from . import main
