2a3c466bbfec2b29a71421852591d22e8a1a1d84
Mathieu THIBAUD <94779022+MathThibaud@users.noreply.github.com>
2025-05-12 14:29:37 +0200

    améliorations

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
+
+    # Importations locales pour éviter les imports circulaires
+    from routes.index import index_bp
+    from routes.entrainement import entrainement_bp
+
+    # Enregistrement des "blueprints"
+    app.register_blueprint(index_bp)
+    app.register_blueprint(entrainement_bp)
+
+    return app
+
 from app import routes
