bf40e936a5e8c258c807a2e62826d264453daaa3
Mathieu THIBAUD <94779022+MathThibaud@users.noreply.github.com>
2025-05-12 13:37:53 +0200

    Tentative avec minimum

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-print("✅ Flask app créée")
-
-# Importer les routes *après* la création de `app`
-from . import main
-
-application = app
-print("✅ Fin de main.py atteinte")
+from app import routes
