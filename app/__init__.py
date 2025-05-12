c759bf7e6c5f21394d236dc1244043f77de1a8e2
Mathieu THIBAUD <94779022+MathThibaud@users.noreply.github.com>
2025-05-12 13:12:04 +0200

    modified:   app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import render_template
+from . import app
 
-app = Flask(__name__)
-
-# On importe les routes (dans main.py)
-from . import main
+@app.route("/")
+def index():
+    return render_template("index.html")
